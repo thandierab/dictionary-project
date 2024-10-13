f47cccdb59a49d1d2cef2f395df93a8da240b7d3
thandierab <thandierabasimane@gmail.com>
2024-10-13 23:04:22 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="thandie-logo">
+          <div>THANDIE</div>
+          <div>R-MANE</div>
+        </div>
+        <p>Welcome to my dictionary app</p>
+      </header>
+    </div>
+  );
+}
